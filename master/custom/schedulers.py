f891a6a6fcb61399ae51ecdf1f493aecb0127828
Itamar Oren <itamarost@gmail.com>
2023-10-31 16:32:37 -0700

    add debug logs

--- master/custom/schedulers.py
+++ master/custom/schedulers.py
@@ -8,6 +8,7 @@ class GitHubPrScheduler(AnyBranchScheduler):
     def __init__(self, *args, stable_builder_names, **kwargs):
         super().__init__(*args, **kwargs)
         self.stable_builder_names = stable_builder_names
+        log.msg(f"GitHubPrScheduler init, stable builder names: {self.stable_builder_names}")
 
     @defer.inlineCallbacks
     def addBuildsetForChanges(self, **kwargs):
@@ -24,6 +25,7 @@ class GitHubPrScheduler(AnyBranchScheduler):
         builder_filter = chdict["properties"].get("builderfilter", None)
         event = chdict["properties"].get("event", None)
         builder_names = kwargs.get("builderNames", self.builderNames)
+        log.msg(f"Builder filter: {builder_filter}, event: {event}, builder names: {builder_names}")
         if builder_filter and builder_names:
             # allow unstable builders only for comment-based trigger
             if event != "issue_comment":
@@ -32,9 +34,11 @@ class GitHubPrScheduler(AnyBranchScheduler):
                     for builder_name in builder_names
                     if builder_name in self.stable_builder_names
                 ]
+                log.msg(f"Filtered builder names: {builder_names}")
             log.msg("Found builder filter: {}".format(builder_filter))
             builder_filter, _ = builder_filter
             matcher = re.compile(builder_filter, re.IGNORECASE)
+            log.msg(f"Builder names pre-matcher: {builder_names}")
             builder_names = [
                 builder_name
                 for builder_name in builder_names
